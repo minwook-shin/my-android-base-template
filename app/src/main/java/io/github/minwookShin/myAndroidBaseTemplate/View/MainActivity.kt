ab33d40b0485beb968b4716654a5a31e81d8897c
minwook-shin <minwook0106@gmail.com>
2018-08-04 17:25:04 +0900

    add custom vertical dialog

--- app/src/main/java/io/github/minwookShin/myAndroidBaseTemplate/View/MainActivity.kt
+++ app/src/main/java/io/github/minwookShin/myAndroidBaseTemplate/View/MainActivity.kt
@@ -4,6 +4,7 @@ import android.databinding.DataBindingUtil
 import android.os.Bundle
 import io.github.minwookShin.myAndroidBaseTemplate.Base.BaseActivity
 import io.github.minwookShin.myAndroidBaseTemplate.Dialog.HorizontalDialog
+import io.github.minwookShin.myAndroidBaseTemplate.Dialog.VerticalDialog
 import io.github.minwookShin.myAndroidBaseTemplate.R
 import io.github.minwookShin.myAndroidBaseTemplate.ViewModel.MainViewModel
 import io.github.minwookShin.myAndroidBaseTemplate.databinding.ActivityMainBinding
@@ -17,15 +18,24 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         bind
         connectViewModel()
-        makeDialog()
+        makeVerticalDialog()
     }
 
     private fun connectViewModel(){
         bind.model = MainViewModel()
     }
 
-    private fun makeDialog(){
+    private fun makeHorizontalDialog(){
         HorizontalDialog.Builder(this)
+                .setMode(HorizontalDialog.MODE_OK_CANCEL)
+                .setTitle("제목")
+                .setMassage("메시지")
+                .setOkayButton("예")
+                .setCancelButton("아니오")
+                .show()
+    }
+    private fun makeVerticalDialog(){
+        VerticalDialog.Builder(this)
                 .setMode(HorizontalDialog.MODE_OK_CANCEL)
                 .setTitle("제목")
                 .setMassage("메시지")
